1b3c2cef3c9284460e80d74d8409d9e15bb08ca1
Abul Hasan <xbeydtha@gmail.com>
2018-04-06 09:44:08 +0200

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+
+
+
+const Titles = () =>(
+
+    <div>
+        <h1> Weather Finder </h1>
+        <p>Find out  the temparature of a certain location in the world and more..  </p>
+
+    </div>
+);
+
+export default Titles;
